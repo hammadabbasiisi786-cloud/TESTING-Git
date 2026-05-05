bfc361a4e084e446ab3d682669d569c02d9ee6fb
Hamad Abbasi <hammadabbasiisi786@gmail.com>
2026-05-06 00:19:57 +0500

    First version of Q1

---
+++ Q1.java
@@ -0,0 +1,7 @@
+public class Q1 {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println("my name is hamad");
+        System.out.println("My patner is hamdan");
+    }
+}
