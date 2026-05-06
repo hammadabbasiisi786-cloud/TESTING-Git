81b450d3f4db68bb8653d481f057738579e3ec95
hamdanahmed21 <hamdanahmed.2004.16@gmail.com>
2026-05-06 12:20:08 +0500

    Hamdan changes kiye hain

--- KSHamdan.java
+++ KSHamdan.java
@@ -3,5 +3,7 @@ public class KSHamdan {
         
         System.out.println("Hello Im hamdan ");
         System.out.println("I will now make changes to this");
+
+        System.out.println("I have corrected the logic")
     }
 }
