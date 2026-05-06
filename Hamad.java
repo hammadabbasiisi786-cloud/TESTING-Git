702511116226cc8450316f5b756a30f33971d8a7
Hamad Abbasi <hammadabbasiisi786@gmail.com>
2026-05-06 09:07:05 +0500

    9:00 am FIRST COMMIT

--- Hamad.java
+++ Hamad.java
@@ -1,7 +1,9 @@
-public class Q1 {
+public class Hamad {
     public static void main(String[] args) {
         System.out.println("Hi");
         System.out.println("my name is hamad");
         System.out.println("My patner is hamdan");
+
+        System.out.println("I will make changes to this ");
     }
 }
---
+++ KSHamdan.java
@@ -0,0 +1,7 @@
+public class KSHamdan {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Im hamdan ");
+        System.out.println("I will now make changes to this");
+    }
+}
