0d63c45113fdb8d278ca5531504e80c7eb30d15f
hamdanahmed21 <hamdanahmed.2004.16@gmail.com>
2026-05-06 12:42:19 +0500

    ABI ABI CHANGE

--- KSHamdan.java
+++ KSHamdan.java
@@ -4,6 +4,8 @@ public class KSHamdan {
         System.out.println("Hello Im hamdan ");
         System.out.println("I will now make changes to this");
 
-        System.out.println("I have corrected the logic")
+        System.out.println("I have corrected the logic");
+        System.out.println("ye changes abi kiye hain");
+
     }
 }
