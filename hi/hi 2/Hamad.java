e48f8643355193c28fd8836ad0c1decb22689671
Fawad Abbasi <fawadabbasiisi@gmail.com>
2026-06-28 22:19:27 +0500

    Resolved

---
+++ hi/hi 2/Hamad.java
@@ -0,0 +1,13 @@
+public class Hamad {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println("my name is hamad");
+        System.out.println("My patner is hamdan");
+
+        System.out.println("I will make changes to this ");
+
+        System.out.println("This is the first change made at 9:22 am (sharp");
+
+        System.out.println("LAtest change by hamad ahgjgt 1:30pm00");
+    }
+}
---
+++ hi/hi 2/KSHamdan.java
@@ -0,0 +1,11 @@
+public class KSHamdan {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Im hamdan ");
+        System.out.println("I will now make changes to this");
+
+        System.out.println("I have corrected the logic");
+        System.out.println("ye changes abi kiye hain");
+        System.out.println("1:35pm");
+    }
+}
