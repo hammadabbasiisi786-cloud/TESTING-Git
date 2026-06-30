0bd505ccb3608a188fd7a7ce375712e4049adf57
Ali Saim Salehzadeh <alisaimpersonal@gmail.com>
2026-06-30 23:53:48 +0500

    maga added

--- Brand New Dir for Java files/Hamad.java
+++ Brand New Dir for Java files/Hamad.java
@@ -1,6 +1,6 @@
 public class Hamad {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        System.out.println("MAGA - MAKE AMERICA GREAT AGAIN");
         System.out.println("my name is hamad");
         System.out.println("My patner is hamdan");
 
